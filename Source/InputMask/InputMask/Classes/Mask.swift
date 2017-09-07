d894be23e12aaeb527d107f40afa499496d8dc81
Vince Cooley <vcooley@users.noreply.github.com>
2017-09-07 17:02:32 -0500

    Try something

--- Source/InputMask/InputMask/Classes/Mask.swift
+++ Source/InputMask/InputMask/Classes/Mask.swift
@@ -18,7 +18,7 @@ import Foundation
  
  - seealso: ```Compiler```, ```State``` and ```CaretString``` classes.
  */
-@objc public class Mask: CustomDebugStringConvertible, CustomStringConvertible {
+@objc public class Mask: NSObject, CustomDebugStringConvertible, CustomStringConvertible {
     
     /**
      ### Result
